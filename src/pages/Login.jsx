7822b21f23343190bf7e4cdc8e337263e3eb8938
jonmanhaes <jonatasjonasjonat@gmail.com>
2022-02-14 16:28:55 -0300

    Requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from '../trivia.png';
+import '../App.css';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      disabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.activateButton);
+  };
+
+  activateButton = () => {
+    const { name, email } = this.state;
+    if (name && email) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  };
+
+  render() {
+    const { name, email, disabled } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+        </header>
+        <form>
+          <input
+            data-testid="input-player-name"
+            value={ name }
+            type="text"
+            name="name"
+            onChange={ this.handleChange }
+            placeholder="Username"
+          />
+          <input
+            data-testid="input-gravatar-email"
+            value={ email }
+            type="email"
+            name="email"
+            onChange={ this.handleChange }
+            placeholder="e-mail"
+          />
+          <button data-testid="btn-play" type="button" disabled={ disabled }>
+            Play
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/store/actions/index.js
@@ -0,0 +1,2 @@
+export const PLAYER_DATA = 'PLAYER_DATA';
+export const saveDataPlayer = (payload) => ({ type: PLAYER_DATA, payload });
